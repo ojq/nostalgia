158048d91508ddf30ce136913e52cecc86b45fde
Aleksandra Angelus <olka@hellonet.pl>
2012-05-11 11:41:31 +0200

    Nostalgia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nostalgia::Application.config.secret_token = '20c35046cca2dcf0a6b72134f1f59b1f78fe5d9dd3f7295b42a26568181be70e27571cd6d5aaeda1a5facc956cd81c1b30ac4d1c4d7b9ecdaf90ee72c07fc53d'
